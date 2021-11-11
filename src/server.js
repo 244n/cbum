c78639ddce3d9e04761f4e654abff8c2e28ba796
Tsuyoshi Nobata <vnz@mini.local>
2021-11-12 01:37:52 +0900

    [add] swagger ui (using auto-generator)

--- src/server.js
+++ src/server.js
@@ -7,9 +7,14 @@ const partsRouter = require("./routers/parts");
 const musclesRouter = require("./routers/muscles");
 const menusRouter = require("./routers/menus");
 
+// swagger
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('../swagger_output.json')
+
 const setupServer = () => {
   // Midleware
   app.use(express.json());
+  app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
   // router
   app.use("/api/parts", partsRouter);
