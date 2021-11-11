0960f36fd3fc7beb274dde9605a802b3af6f56fb
Tsuyoshi Nobata <vnz@mini.local>
2021-11-11 14:19:14 +0900

    [add] initializer & test

---
+++ tests/initialize.js
@@ -0,0 +1,15 @@
+const config = require("../config");
+const knex = require("knex")(config.db);
+
+const ignoreError = () => {
+  // do nothing
+};
+
+const clearTable = (tableName) =>
+  knex(tableName)
+    .del()
+    .catch(ignoreError);
+
+const tables = ["muscles"];
+
+Promise.all(tables.map(clearTable)).then(process.exit);
---
+++ tests/test.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-console */
+const { expect, assert } = require("chai");
+const config = require("../config");
+const knex = require("knex")(config.db);
+const models = require("../models")(knex);
+
+describe("users", () => {
+  describe("setup", () => {
+    it("able to connect to database", () =>
+      knex
+        .raw("select 1+1 as result")
+        .catch(() => assert.fail("unable to connect to db")));
+
+    it("has run the initial migrations", () =>
+      knex("muscles")
+        .select()
+        .catch(() => assert.fail("muscles table is not found.")));
+  });
+});
